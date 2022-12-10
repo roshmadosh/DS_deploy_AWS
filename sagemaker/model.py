dfe3141c7355ac764c5f01440b9d46e53d1f6ab6
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-12-10 13:05:50 -0500

    last commit incorrect, this time data ready for modeling

--- sagemaker/model.py
+++ sagemaker/model.py
@@ -1,15 +1,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
 # *** IMPORT DATA ***
 movies_df = pd.read_csv('~/brainstation/capstone/DS_deploy_AWS/sagemaker/movies.csv')
 ratings_df = pd.read_csv('~/brainstation/capstone/DS_deploy_AWS/sagemaker/ratings.csv')
 
-
 # merge dataframes
 df = pd.merge(ratings_df, movies_df, how='inner', on='movieId')
+
+# remove features that won't be included in our logit
+df = df.drop(['userId', 'movieId', 'timestamp'], axis=1)
+
+'''cross reference original df to get genres for each movie. we're doing this
+because groupby will remove non numeric columns.'''
+DELIMITER = "hiroshisdelimeter"
+cross_ref = df['title'] + [DELIMITER] + df['genres']
+genres_column = [row.split(DELIMITER) for row in cross_ref.unique()]
+genres_df = pd.DataFrame(genres_column, columns=['title', 'genres'])
+
+# group by title and get the mean rating for each movie
+df = df.groupby('title').mean()
+
+# merge genres column with grouped dataframe
+df = df.merge(genres_df, how='inner', on='title')
+print(df)
 
 # create list of unique genres
 genres_array = np.array([genres.split('|') for genres in df['genres']])
@@ -17,7 +33,6 @@ flattened_genre_array = genres_array.flatten()
 flattened_more = [genre[0] for genre in flattened_genre_array]
 
 unique_genres = set(flattened_more)
-
 
 # create a list of genre one hot encodings
 genre_OHEs = list()
@@ -27,11 +42,16 @@ for row in genres_array:
 
 # convert genre one hot encodings to dataframe
 genres_df = pd.DataFrame(genre_OHEs, columns=list(unique_genres))
-print(genres_df.head())
+
 
 # merge genre OHEs to original dataframe
-
 df = pd.concat((df, genres_df), axis=1)
-print(df)
+print(df.loc[0, :])
+
+# *** MODEL ***
+
+
+
+
 
 
